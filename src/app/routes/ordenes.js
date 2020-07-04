ca44485041426c8c33763069c0bfe3185b0167d9
belkixrequejo <belkix.requejo@pucese.edu.ec>
2020-07-04 04:53:08 -0500

    list orden

--- src/app/routes/ordenes.js
+++ src/app/routes/ordenes.js
@@ -4,11 +4,13 @@ const dbConnection = require("../../config/dbConnection");
 module.exports = app => {
 
     const connection = dbConnection();
+
     app.get('/listaorden', (req, res) => {
         connection.query('SELECT * FROM orders', (err, result) => {
-            res.render('views/ordenes', {
+            res.render('views/orden', {
                 class3: result
             });
         });
     });
+
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const app = require("./config/server");
 require("./app/routes/view2")(app);
 require("./app/routes/view1")(app);
 require("./app/routes/home")(app);
-require("./app/routes/formulario")(app);
 require("./app/routes/ordenes")(app);
+require("./app/routes/formulario")(app);
+
 
 // starting the server
 app.listen(app.get("port"), () => {
